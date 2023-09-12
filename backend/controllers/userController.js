c5ece59a393d8202e8e40e3683708a733650f26c
H.Huseyin Sahin <huseyin.sahin.3321@gmail.com>
2023-09-12 12:53:59 +0300

    feat: admin users functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -125,28 +125,67 @@ const updateUserProfile = asyncHandler(async (req, res) => {
 // @route   GET /api/users
 // @access  Private/ADMIN
 const getUsers = asyncHandler(async (req, res) => {
-  res.send("Get users");
+  const users = await User.find({});
+  res.status(200).json(users);
 });
 
 // @desc    Get User By ID
 // @route   GET /api/users/:id
 // @access  Private/ADMIN
 const getUserByID = asyncHandler(async (req, res) => {
-  res.send("Get user by id");
+  const user = await User.findById(req.params.id).select("-password");
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });
 
 // @desc    Update User
 // @route   PUT /api/users/:id
 // @access  Private/ADMIN
 const updateUser = asyncHandler(async (req, res) => {
-  res.send("Update user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = Boolean(req.body.isAdmin);
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });
 
 // @desc    Delete User
 // @route   DELETE /api/users/:id
 // @access  Private/ADMIN
 const deleteUser = asyncHandler(async (req, res) => {
-  res.send("Delete user");
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    // Prevent deleting admin users
+    if (user.isAdmin) {
+      res.status(403);
+      throw new Error("Can't delete admin user");
+    }
+    await User.deleteOne({ _id: user._id });
+    res.status(200).json({ message: "User deleted successfully" });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
 });
 
 export {
